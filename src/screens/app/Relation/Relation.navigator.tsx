9b6f84e2355ec7877b8bf1cd6353199fd3db5254
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2020-03-23 21:35:17 +0100

    feat(auth): add option to logout

--- src/screens/app/Relation/Relation.navigator.tsx
+++ src/screens/app/Relation/Relation.navigator.tsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import { RelationScreen } from './RelationScreen'
 import { getStackOptions } from '../../../res/stackNavigation'
 import { IconButton } from '../../../library/components/IconButton'
+import { AuthContext } from '../../auth'
 
 const Stack = createStackNavigator()
 
-export const RelationNavigator = () => (
-  <Stack.Navigator initialRouteName="Relation">
-    <Stack.Screen
-      name="Relation"
-      component={RelationScreen}
-      options={getStackOptions({
-        headerRight: () => (
-          <IconButton
-            iconName="broken_heart"
-            width={25}
-            height={25}
-            fill="white"
-          />
-        ),
-      })}
-    />
-  </Stack.Navigator>
-)
+export const RelationNavigator = () => {
+  const { setIsConnected } = useContext(AuthContext)
+
+  const onLogout = () => setIsConnected(false)
+
+  return (
+    <Stack.Navigator initialRouteName="Relation">
+      <Stack.Screen
+        name="Relation"
+        component={RelationScreen}
+        options={getStackOptions({
+          headerRight: () => (
+            <IconButton
+              iconName="broken_heart"
+              width={25}
+              height={25}
+              fill="white"
+              onAction={onLogout}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  )
+}
