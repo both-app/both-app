d9237a9d474af683118d6c72c40f73c5e3720623
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2020-03-26 10:02:29 +0100

    chore: init sentry

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import 'react-native-gesture-handler'
 
 import React from 'react'
+import * as Sentry from 'sentry-expo'
+import Constants from 'expo-constants'
 import { NavigationContainer } from '@react-navigation/native'
 import { AuthContextProvider } from './src/screens/auth'
 import { Main } from './src/Main'
+
+Sentry.init({
+  dsn: 'https://3247720673cf460bb7979a5e0554d98f@sentry.io/5175612',
+  enableInExpoDevelopment: true,
+  debug: true,
+})
+
+Sentry.setRelease(Constants.manifest.revisionId)
 
 export default () => (
   <AuthContextProvider>
