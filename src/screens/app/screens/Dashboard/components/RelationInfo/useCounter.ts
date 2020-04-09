09af0e23c410fecc374227f4ee87b84f593e06c3
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2020-04-09 09:34:24 +0200

    feat: improve the counter

--- src/screens/app/screens/Dashboard/components/RelationInfo/useCounter.ts
+++ src/screens/app/screens/Dashboard/components/RelationInfo/useCounter.ts
@@ -3,18 +3,24 @@ import { useState, useEffect } from 'react'
 import { wait } from 'res/utils'
 
 export const useCounter = (value: number) => {
+  const [isInited, setIsInit] = useState(false)
   const [newValue, setNewValue] = useState(0)
 
   useEffect(() => {
     const changeValue = async () => {
+      if (value > 0 && newValue === 0 && !isInited) {
+        setIsInit(true)
+        return setNewValue(value)
+      }
+
       if (newValue !== value) {
         await wait(150)
 
         if (value > newValue) {
-          setNewValue(newValue + 1)
-        } else {
-          setNewValue(newValue - 1)
+          return setNewValue(newValue + 1)
         }
+
+        return setNewValue(newValue - 1)
       }
     }
 
