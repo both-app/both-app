4f56fac552cf65c53b74940c9e4d5625420f7859
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2020-08-19 10:06:32 +0200

    feat: add new component AddModal

--- src/screens/app/App.navigator.tsx
+++ src/screens/app/App.navigator.tsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import { Icon } from 'library/components/Icon'
 import { TabBar } from './components/TabBar'
+import { AddModal } from './components/AddModal'
 
 import { HomeScreen } from './screens/Home'
 import { RelationScreen } from './screens/Relation'
@@ -25,94 +26,102 @@ const ROUTES: Record<string, AppRoute> = {
 }
 
 const MainNavigator = () => {
+  const [addModalIsVisible, setAddModalIsVisible] = useState(false)
   const { appRoutesWithBadge } = useContext(AppNavigatorContext)
 
   return (
-    <Tab.Navigator initialRouteName={ROUTES.HOME} tabBar={TabBar}>
-      <Tab.Screen
-        name={ROUTES.HOME}
-        component={HomeScreen}
-        options={{
-          tabBarVisible: true,
-          tabBarIcon: ({ size, color }) => (
-            <Icon
-              iconName="home"
-              width={size}
-              height={size}
-              style={{ color }}
-            />
-          ),
-        }}
+    <>
+      <Tab.Navigator initialRouteName={ROUTES.HOME} tabBar={TabBar}>
+        <Tab.Screen
+          name={ROUTES.HOME}
+          component={HomeScreen}
+          options={{
+            tabBarVisible: true,
+            tabBarIcon: ({ size, color }) => (
+              <Icon
+                iconName="home"
+                width={size}
+                height={size}
+                style={{ color }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ROUTES.RELATION}
+          component={RelationScreen}
+          options={{
+            tabBarVisible: true,
+            // @ts-ignore
+            tabBarBadge: appRoutesWithBadge.Relation,
+            tabBarIcon: ({ size, color }) => (
+              <Icon
+                iconName="users"
+                width={size}
+                height={size}
+                style={{ color }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ROUTES.ADD_TASK}
+          component={AddTaskNavigator}
+          listeners={() => ({
+            tabPress: (e) => {
+              e.preventDefault()
+              setAddModalIsVisible(true)
+            },
+          })}
+          options={{
+            tabBarVisible: true,
+            tabBarIcon: ({ size }) => (
+              <Icon
+                iconName="plus"
+                width={size}
+                height={size}
+                style={{ color: 'white' }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ROUTES.LEADERBOARD}
+          component={LeaderboardScreen}
+          options={{
+            tabBarVisible: true,
+            tabBarIcon: ({ size, color }) => (
+              <Icon
+                iconName="award"
+                width={size}
+                height={size}
+                style={{ color }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ROUTES.SETTINGS}
+          component={SettingsNavigator}
+          options={{
+            tabBarVisible: true,
+            tabBarIcon: ({ size, color }) => (
+              <Icon
+                iconName="settings"
+                width={size}
+                height={size}
+                style={{ color }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+
+      <AddModal
+        isVisible={addModalIsVisible}
+        onClose={() => setAddModalIsVisible(false)}
       />
-      <Tab.Screen
-        name={ROUTES.RELATION}
-        component={RelationScreen}
-        options={{
-          tabBarVisible: true,
-          // @ts-ignore
-          tabBarBadge: appRoutesWithBadge.Relation,
-          tabBarIcon: ({ size, color }) => (
-            <Icon
-              iconName="users"
-              width={size}
-              height={size}
-              style={{ color }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={ROUTES.ADD_TASK}
-        component={AddTaskNavigator}
-        listeners={({ navigation }) => ({
-          tabPress: (e) => {
-            e.preventDefault()
-            navigation.navigate(ROUTES.ADD_TASK_MODAL)
-          },
-        })}
-        options={{
-          tabBarVisible: true,
-          tabBarIcon: ({ size }) => (
-            <Icon
-              iconName="plus"
-              width={size}
-              height={size}
-              style={{ color: 'white' }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={ROUTES.LEADERBOARD}
-        component={LeaderboardScreen}
-        options={{
-          tabBarVisible: true,
-          tabBarIcon: ({ size, color }) => (
-            <Icon
-              iconName="award"
-              width={size}
-              height={size}
-              style={{ color }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={ROUTES.SETTINGS}
-        component={SettingsNavigator}
-        options={{
-          tabBarVisible: true,
-          tabBarIcon: ({ size, color }) => (
-            <Icon
-              iconName="settings"
-              width={size}
-              height={size}
-              style={{ color }}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    </>
   )
 }
 
---
+++ src/screens/app/components/AddModal/AddModal.tsx
@@ -0,0 +1,84 @@
+import React, { FC } from 'react'
+import RModal from 'react-native-modal'
+import { StyleSheet, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import { CardButton } from 'library/components/CardButton'
+import { IconButton } from 'library/components/IconButton'
+
+import { colors } from 'res/colors'
+
+interface AddModalProps {
+  isVisible: boolean
+  onClose: () => void
+}
+
+export const AddModal: FC<AddModalProps> = ({ isVisible, onClose }) => {
+  const navigation = useNavigation()
+
+  return (
+    <RModal
+      isVisible={isVisible}
+      style={styles.modal}
+      animationIn="slideInUp"
+      animationOut="slideOutDown"
+      onBackdropPress={onClose}
+    >
+      <View style={styles.container}>
+        <CardButton
+          emoji="📦"
+          title="Ajouter une tâche"
+          subtitle="83 tâches disponibles"
+          containerStyle={styles.addTaskButton}
+          onAction={() => {
+            navigation.navigate('AddTaskModal')
+            onClose()
+          }}
+        />
+        <CardButton
+          emoji="👋"
+          title="Demander de l'aide à ton partenaire"
+          subtitle="3 étapes rapides"
+        />
+
+        <IconButton
+          size={64}
+          iconName="close"
+          onAction={onClose}
+          iconColor="white"
+          buttonStyle={styles.closeButton}
+        />
+      </View>
+    </RModal>
+  )
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    margin: 0,
+    backgroundColor: colors.skin100,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    flex: 0,
+    bottom: 0,
+    position: 'absolute',
+    width: '100%',
+  },
+  container: {
+    paddingTop: 24,
+    paddingLeft: 24,
+    paddingRight: 24,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: colors.skin100,
+    alignItems: 'center',
+  },
+  addTaskButton: {
+    marginBottom: 8,
+  },
+  closeButton: {
+    marginTop: 40,
+    backgroundColor: colors.dark200,
+    marginBottom: 32,
+  },
+})
